6f2f9d459ee90e35dfb5a24db50f0193f03ac106
fault2004 <46396282+fault2004@users.noreply.github.com>
2023-12-04 00:07:10 +0700

    Updated Version => 6.4.1_next
    Change OTA URL to this repo

--- src/urihandler/otahandler.c
+++ src/urihandler/otahandler.c
@@ -27,8 +27,8 @@ typedef struct
     int http_code;
 } http_handler_data_t;
 
-static const char *DEFAULT_URL = "https://raw.githubusercontent.com/dchristl/esp32_nat_router_extended/releases-production/";
-static const char *DEFAULT_URL_CANARY = "https://raw.githubusercontent.com/dchristl/esp32_nat_router_extended/releases-staging/";
+static const char *DEFAULT_URL = "https://raw.githubusercontent.com/fault2004/esp32_nat_router_extended_nextdns/releases-production/";
+static const char *DEFAULT_URL_CANARY = "https://raw.githubusercontent.com/fault2004/esp32_nat_router_extended_nextdns/releases-staging/";
 
 void appendToLog(const char *message)
 {
